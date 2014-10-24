7df0c6efb9c3507111c69c62a756e5ed125030a7
torrenegras <xoaco@outlook.com>
2014-10-24 16:48:37 -0500

    BUG FIX PUSH NOTIFICATIONS OPEN DEFAULT

---
+++ src/com/xoaquin/r07d/MyCustomReceiver3.java
@@ -0,0 +1,20 @@
+package com.xoaquin.r07d;
+
+import android.content.Context;
+import android.content.Intent;
+
+import com.parse.ParsePushBroadcastReceiver;
+
+
+
+public class MyCustomReceiver3 extends ParsePushBroadcastReceiver {
+
+    @Override
+    public void onPushOpen(Context context, Intent intent) {
+        
+        Intent i = new Intent(context, MainActivity.class);
+        i.putExtras(intent.getExtras());
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(i);
+    }
+}
